536be44cb131372d6b72b762334d102ae03fbd19
Tasnime66 <tasnimear2@gmail.com>
2024-04-29 16:41:38 +0200

    Ajout du programme (logs du parefeu)

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadFirewallLogs_Click(object sender, RoutedEventArgs e)
+        {
+            LogTextBox.IsReadOnly = true;
+            string firewallLogFilePath = @"C:\Windows\System32\LogFiles\Firewall\pfirewall.log";
+
+            try
+            {
+                string tempFilePath = Path.Combine(Path.GetTempPath(), "pfirewall_copie.log");
+
+                // Copier le fichier pfirewall.log vers un emplacement temporaire
+                File.Copy(firewallLogFilePath, tempFilePath, true);
+
+                // Lire le fichier temporaire
+                string[] logLines = File.ReadAllLines(tempFilePath);
+
+                LogTextBox.Clear();
+
+                foreach (string line in logLines)
+                {
+                    LogTextBox.AppendText(line + Environment.NewLine);
+                }
+
+                // delete  fichier temporaire après lecture
+                File.Delete(tempFilePath);
+            }
+            catch (Exception ex)
+            {
+                LogTextBox.AppendText($"Une erreur s'est produite : {ex.Message}");
+            }
+        }
+    }
+}
